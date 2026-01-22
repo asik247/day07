e3b896d0a209041d91a1295efd305ca6fdbfd475
Asikur Rahaman <mdasikur5893@gmail.com>
2026-01-22 16:51:24 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById('para').innerHTML = "ima js code hre and chage this html code okkk."
+
+
+let money = 0;
+if(money>300){
+    console.log('got to cineplex');
+}else{
+    if(money>100){
+        console.log('what the fuchka');
+    }if(money>0){
+        console.log('tui kola kine kha');
+    }
+    else{
+        console.log('tui kisui pabi na');
+    }
+}
